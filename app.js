c4760e5c1125412a52b70ec83954c509ce923074
Alina M <alinaamoskalenko@gmail.com>
2022-11-29 12:39:59 -0800

    initial board and notes plan

---
+++ app.js
@@ -0,0 +1,25 @@
+alert('New Game');
+// create an array
+const gameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+// Chunks to Code
+
+// let player choose their piece (tack or toe)
+// computer auto pick the remaining piece
+
+// player choice changes gameBoard
+
+// computer random choice
+// computer choice to change array
+
+//choice corrolate to change on gameBoard
+// event listener player click
+
+// different patterns of winning
+// add who won
+// tick crawls over winning pattern
+//alert pop up to reset game
